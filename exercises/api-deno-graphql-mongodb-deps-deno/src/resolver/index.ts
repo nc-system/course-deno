cf30110ffe55b8978633394f774bc29d4f459900
Andres Giraldo Arenas <andresganc@gmail.com>
2022-09-26 09:58:13 -0500

    Structure & Files

---
+++ exercises/api-deno-graphql-mongodb-deps-deno/src/resolver/index.ts
@@ -0,0 +1,6 @@
+export * from './user.ts';
+import { UserResolvers } from "./user.ts";
+//import { AuthorResolvers } from "./author.ts";
+
+//export const resolvers = [ UserResolvers, AuthorResolvers ];
+export const resolvers = [ UserResolvers ];
